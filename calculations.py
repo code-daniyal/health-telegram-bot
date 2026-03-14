8876d5d696342fe341203d4548bd47d607068b77
Daniyal <bambalbiedgar@gmail.com>
2026-03-14 20:43:33 +0500

    fix git conflict in calculations

--- calculations.py
+++ calculations.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 def calculate_bmi(weight, height_cm):
     height_m = height_cm / 100
     bmi = weight / (height_m ** 2)
@@ -21,7 +21,7 @@ def calculate_bmi(weight, height_cm):
 
 def calculate_calories(weight, height, age):
     bmr = 10 * weight + 6.25 * height - 5 * age + 5
-=======
+
 def calculate_bmi(weight, height_cm):
     height_m = height_cm / 100
     bmi = weight / (height_m ** 2)
@@ -44,5 +44,4 @@ def calculate_bmi(weight, height_cm):
 
 def calculate_calories(weight, height, age):
     bmr = 10 * weight + 6.25 * height - 5 * age + 5
->>>>>>> 29093cf4bcf2d7ee60c67f712405b99b90c1b8a1
     return int(bmr * 1.4)
